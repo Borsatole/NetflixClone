c09e0989d734929a1ef45646398d291dd1d6c4ef
F Santos <borsatole@gmailcom>
2025-01-11 16:09:56 -0300

    nova imagem

--- src/components/FilmeTopoPagina.jsx
+++ src/components/FilmeTopoPagina.jsx
@@ -21,8 +21,13 @@ function FilmeTopoPagina() {
             </div>
 
             <div className='buttons'>
-              <a href='#'><div>Play</div></a>
-              <a href='#'><div>Mais Informações</div></a>
+              <a href='#'>
+                <div> 
+                  <img src="https://raw.githubusercontent.com/Borsatole/NetflixClone/8d1731702ab8bf26466d99a38b6374da9deb5c82/public/images/icons/Play.svg" alt="" />Play</div>
+                </a>
+              <a href='#'>
+                <div> <img src="https://raw.githubusercontent.com/Borsatole/NetflixClone/8d1731702ab8bf26466d99a38b6374da9deb5c82/public/images/icons/Play.svg" alt="" />Mais Informações</div>
+                </a>
             </div>
           </section>
           
